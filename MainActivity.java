9c8b1b31026505255b0086c461f363b785476986
Suntanman3 <benjamin.buttera@bell.net>
2017-03-22 16:18:30 -0400

    Update MainActivity.java
    Updated so buttons

--- MainActivity.java
+++ MainActivity.java
@@ -1,15 +1,26 @@
-package com.example.benjamin.blabfridgeapp;
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.ArrayList;
+
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     UDPListener listener;
     UDPSender sender;
-    String foodList = "";
+
+    public String foodList;
+    public static final char[] blankTagCodeCharArray = {'0','0','0','0','0','0','0','0','0','0'};
+    public static final int fridgeControllerPort = 1111;
+    private DatagramSocket sock;
+    private InetAddress fridgeControllerInetAddress;
     //This app will eventually receive an update from the fridge, and interpret that signal to
     // an update message. This will be displayed on the screen of the phone.
     //
@@ -25,60 +36,93 @@ public class MainActivity extends AppCompatActivity {
     public MainActivity() {
         listener = new UDPListener();
         sender = new UDPSender();
-
     }
-    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            sock = new DatagramSocket();
+        }catch(SocketException e){
+            System.out.println("Socket Error");
+        }
     }
 
     /** Called when the user clicks the send button */
 
     public void listFridge(View view){
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        sender.sendReceivePacket(0);
-        // Read from file, make into a list to display.
+        ArrayList<FoodItem> arrayList = getItemsExpiringBefore(0);
+        sortList(arrayList);
+        foodList = listFood(arrayList);
+        intent.putExtra("listFood",foodList);
         startActivity(intent);
     }
     public void listExpiring(int expiry){
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        sender.sendReceivePacket(expiry);
-        //Read from file, make into a list to display.
-        FoodItem[] array = new FoodItem[0];
-        //Read from file
-        sortList(array);
-        listFood(array);
+        ArrayList<FoodItem> arrayList = getItemsExpiringBefore(expiry);
+        sortList(arrayList);
+        foodList = listFood(arrayList);
+        intent.putExtra("listFood",foodList);
         startActivity(intent);
     }
-    public void listFood(FoodItem[] array){
-        for(int i=0;i<array.length; i++){
-            if(i!=array.length){
-                foodList += "/n";
-            }
-            foodList += array[i].toString();
+    public ArrayList<FoodItem> getItemsExpiringBefore(int days){ //this should be called by the actionEvent created by a button press
+        byte[] buf = new byte[100];
+        buf[0] = '9';
+        buf[1] = FoodItem.opcodeDelimiter.getBytes()[0];
+        byte[] daysAsBytes = Integer.toString(days).getBytes();
+        System.arraycopy(daysAsBytes, 0, buf, 2, daysAsBytes.length);
+        DatagramPacket p = new DatagramPacket(buf, buf.length, fridgeControllerInetAddress, fridgeControllerPort);
+        try{
+            sock.send(p);
+        } catch(IOException e){
+            System.out.println("Error sending on socket");
         }
+
+        ArrayList<FoodItem> items = new ArrayList<FoodItem>();
+
+        while(true){
+            try{
+                sock.receive(p);
+            } catch(IOException e){
+                System.out.println("Error receiving on socket");
+            }
+            buf = p.getData();
+            if (buf[0] == '9') break; //we got the last packet, exit.
+            items.add(FoodItem.getFoodItemFromByteArray(blankTagCodeCharArray, buf));
+        }
+        return items;
     }
-    public void sortList(FoodItem[] array){
+    public String listFood(ArrayList<FoodItem> array){
+        String list = "";
+        for(int i=0;i<array.size(); i++){
+            if(i!=0){
+                list += "/n";
+            }
+            list += array.get(i).toString();
+        }
+        return list;
+    }
+    public void sortList(ArrayList<FoodItem> array){
         int k = 0;
         while(k==0){
-            int size = array.length;
+            int size = array.size();
             if(size == 1){
                 k = 1;
             }
             for(int i=0; i<size-1; i++){
-                FoodItem a = array[i];
-                FoodItem b = array[i+1];
+                FoodItem a = array.get(i);
+                FoodItem b = array.get(i+1);
                 if(a.expiresInDays()>b.expiresInDays()){
                     FoodItem temp = a;
-                    array[i] = b;
-                    array[i+1] = temp;
+                    array.set(i,b);
+                    array.set(i+1,temp);
                 }
             }
             int dummyVar = 0;
             for(int j=0; j<size-1; j++){
-                if(array[j].expiresInDays()<=array[j+1].expiresInDays()){
+                if(array.get(j).expiresInDays()<=array.get(j+1).expiresInDays()){
                     dummyVar++;
                 }
             }
