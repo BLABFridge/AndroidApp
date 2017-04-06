5ed954476f025bf5b2597fc89bce96927b95bbdd
Suntanman3 <benjamin.buttera@bell.net>
2017-04-06 19:55:52 -0400

    Update UDPListener.java

--- UDPListener.java
+++ UDPListener.java
@@ -1,3 +1,5 @@
+package com.example.benjamin.blabfridgeapp;
+
 /**
  * Created by Benjamin on 2017-03-22.
  */
@@ -6,6 +8,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 import java.net.DatagramSocket;
 import java.net.DatagramPacket;
@@ -59,7 +62,6 @@ class UDPListener extends Thread{
             p.setSocketAddress(p.getSocketAddress()); //this might actually be redundant (it certainly looks redundant), prep the packet for responding to the sender
             switch (buf[0]) {
                 case '5':  //this is a notification from the fridgecontroller, create a notification and send it
-                    //c.notify(getStringFromByteArray(buf, 1));
                     String fullPacketString = new String(buf);
                     String notificationString = fullPacketString.split(FoodItem.matchRegexOpcodeDelimiter)[1];
                     send5Notification(notificationString);
@@ -74,18 +76,7 @@ class UDPListener extends Thread{
                     }
                     break;
                 case '6'://whatever was scanned does not exist, open a prompt allowing the user to input credentials, to be sent back in a 1 packet
-
                     send6Notification(p);
-
-                    FoodItem foodItem = NameActivity.foodItem;
-                    byte[] foodItemAsByteArray = foodItem.to1Packet(); //= FoodItemCeator.createFoodItem().to1Packet(); //BEN createFoodItem() is whatever method you decide to call to get the user to create a food item
-                    p.setData(foodItemAsByteArray);
-                    try {
-                        sock.send(p);
-                    } catch (IOException e) {
-                        System.out.println("Failed to send a packet");
-                    }
-
                     break;
                 default:
                     break;
@@ -98,39 +89,35 @@ class UDPListener extends Thread{
         nBuilder.setSmallIcon(R.drawable.sym_def_app_icon);
         nBuilder.setContentTitle("BlabFridge");
         nBuilder.setContentText(string);
-        Intent resultIntent = new Intent(context, DisplayMessageActivity.class);
+        nBuilder.setAutoCancel(true);
+        nBuilder.setVibrate(new long[] {1000,1000});
+        nBuilder.setLights(Color.GREEN,3000,3000);
 
-        PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, 0);
+        Intent resultIntent = new Intent(context, MainActivity.class);
+
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         nBuilder.setContentIntent(resultPendingIntent);
         NotificationManager nNotificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
         nNotificationManager.notify(001, nBuilder.build());
         System.out.println("5 Notification Recieved.");
-
     }
     public void send6Notification(DatagramPacket p){
         NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(context);
         nBuilder.setSmallIcon(R.drawable.sym_def_app_icon);
         nBuilder.setContentTitle("BlabFridge");
         nBuilder.setContentText("Scanned Item Does Not Exist");
-        Intent resultIntent = new Intent(context, DisplayMessageActivity.class);
+        nBuilder.setAutoCancel(true);
+        nBuilder.setVibrate(new long[] {1000,1000});
+        nBuilder.setLights(Color.RED,3000,3000);
+
+        Intent resultIntent = new Intent(context, NameActivity.class);
+        resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         resultIntent.putExtra("Server Port", p.getPort());
 
-        PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, 0);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         nBuilder.setContentIntent(resultPendingIntent);
         NotificationManager nNotificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
         nNotificationManager.notify(002, nBuilder.build());
         System.out.println("6 Notification Recieved.");
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        switch(requestCode){
-            case(MY_CHILD_ACTIVITY):{
-                if(resultCode == Activity.RESULT_OK){
-                    String result = ;
-                }
-                break;
-            }
-        }
     }
 }
